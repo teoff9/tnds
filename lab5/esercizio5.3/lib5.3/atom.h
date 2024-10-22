8a065bb4c04b1dbc80998a64c7c46edf153549c1
Matteo Fava <matteo.luca.fava@gmail.com>
2024-10-22 20:03:47 +0200

    esercizio5.3/

---
+++ lab5/esercizio5.3/lib5.3/atom.h
@@ -0,0 +1,30 @@
+//22.10.24 by Matteo Fava
+//Point3D
+#pragma once
+#include "particle.h"
+#include <iostream>
+#include <format>
+using namespace std;
+
+class Electron : public Particle {
+    public: 
+        //costructors
+        Electron() : Particle{9.1093826e-31, -1.60217653e-19} {};
+        //methods
+        void print() const ;
+
+};
+
+class Proton : public Particle {
+    public: 
+        //costructors
+        Proton() : Particle{1.67262192595e-27, 1.60217653e-19} {};
+        //methods
+        void print() const ;
+
+};
+
+void Proton::print() const { cout << format("Proton: (mass {}, charge {})", mass, charge);}
+
+
+void Electron::print() const { cout << format("Electron: (mass {}, charge {})", mass, charge);}
