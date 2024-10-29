d1f4ef3f6c0468c34be0678818ccb39792d8fd8b
Matteo Fava <matteo.luca.fava@gmail.com>
2024-10-29 21:55:25 +0100

    esercizio6.2

---
+++ lab6/esercizio6.2/esercizio.6.2.cpp
@@ -0,0 +1,36 @@
+//29.10.24 by Matteo Fava
+
+#include "lib6.2/math.h"
+#include "lib6.2/functions.h"
+#include "lib6.2/tests.h"
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <cstdlib>
+#include <format>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    test_zeroes();
+    if (argc != 4 ){
+        cerr << format("Arguments incorrect: {} x_min x_max prec\n", argv[0]);
+        exit(-1);
+    }
+    double a = stod(argv[1]);
+    double b = stod(argv[2]);
+    double prec = stod(argv[3]);
+    Parabola p{3,5,-2};
+    Bisection solv;
+    try {
+        double x0 = solv.find_root(p, a, b, prec);
+        cout << "Root found: x = " << fixed << setprecision(-log10(prec)) << x0 << endl;
+    } catch(int e) {
+        if (e == -1) {
+            cout << "Root not found: wrong interval!" << endl;
+        } else {
+            cout << "Root not found: exceeded max number of iterations!" << endl;
+        }
+    }
+
+    return 0;
+}
