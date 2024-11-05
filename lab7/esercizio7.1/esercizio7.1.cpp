cae9f8ca0d88bcd20c54c94affb59e81008388a6
Matteo Fava <matteo.luca.fava@gmail.com>
2024-11-05 16:29:48 +0100

    esercizio7.1/

--- lab7/esercizio7.1/esercizio7.1.cpp
+++ lab7/esercizio7.1/esercizio7.1.cpp
@@ -4,11 +4,25 @@
 #include <cmath>
 #include <vector>
 #include <format>
+#include <numbers>
+#include "lib7.1/integral.h"
+#include "lib7.1/tests.h"
 using namespace std;
 
-const double pi = 3.14159265359;
+double g(double x) {
+    return x*sin(x);
+}
 
 int main() {
+    test_integral();
+    Simpson s;
+    double true_value{1};
+
+    vector<int> steps = {10, 50, 100, 500, 1000};
+    for (int i = 0; i<ssize(steps); i++) {
+        double v{s.integrate(0, numbers::pi/2, steps[i], g)};
+        cout << format("Step / Integral / Error : {} | {} | {} \n", steps[i], v, fabs(true_value-v));
+    }
     
     return 0;
 }
