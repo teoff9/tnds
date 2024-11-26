822fa811036e500ce732c61e62803747f49f1af1
Matteo Fava <matteo.luca.fava@gmail.com>
2024-11-26 16:47:44 +0100

    random and es10.0

---
+++ lab10/esercizio10.0/esercizio10.0.cpp
@@ -0,0 +1,64 @@
+//26.11.2024 by Matteo Fava
+
+#include "lib10.0/gplot++.h"
+#include "lib10.0/random.h"
+#include "lib10.0/test_random.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    //tests
+    test_random();
+
+    //Variable
+    vector<double> v(10000);
+    Gnuplot plt;
+    RandomGen gen{1};
+    const int n_bins{100};
+    plt.multiplot(2,2, "Distribuzioni");
+    
+    //Uniform
+    for (int i{0}; i<10000; i++) {
+        v[i]=gen.unif(5,10);
+    }
+    plt.histogram(v, n_bins);
+    plt.set_title("Uniform (5,10)");
+    plt.set_xrange(4,11);
+    plt.set_yrange(0,NAN);
+    plt.show();
+
+    //Exponential
+    for (int i{0}; i<10000; i++) {
+        v[i]=gen.expo(5);
+    }
+    plt.histogram(v, n_bins);
+    plt.set_title("Exponential 5");
+    plt.set_yrange(0,NAN);
+    plt.set_xrange(-NAN, NAN);
+    plt.show();
+    
+    //Gaussian Box-Muller
+    for (int i{0}; i<10000; i++) {
+        v[i] = gen.gauss(0,1);
+    }
+    plt.histogram(v, n_bins);
+    plt.set_title("Gaussian Box-Muller m=0,s=1");
+    plt.set_yrange(0,NAN);
+    plt.set_xrange(-10, 10);
+    plt.show();
+
+    //Gaussian aer
+    for (int i{0}; i<10000; i++) {
+        v[i]=gen.gauss_aer(0,1);
+    }
+    plt.histogram(v, n_bins);
+    plt.set_title("Gaussian A&R m=0,s=1");
+    plt.set_yrange(0,NAN);
+    plt.set_xrange(-10,10);
+    plt.show();
+
+    cout << "Showed graph! Exiting..\n";
+
+    return 0;
+}
