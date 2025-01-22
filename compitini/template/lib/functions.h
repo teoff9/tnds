83ba7c5ec4fe7df1e2fa6c73d3a0a5580ee0ae0a
Matteo Fava <matteo.luca.fava@gmail.com>
2025-01-22 15:04:16 +0100

    new template

---
+++ compitini/template/lib/functions.h
@@ -0,0 +1,12 @@
+//23.01.2024 by Matteo Fava
+//Questo file contiene le funzioni create per l'esame
+
+#pragma once
+
+#include "gplot++.h"
+#include <iostream>
+#include <format>
+#include <cmath>
+#include <cassert>
+using namespace std;
+
--- compitini/template/main.cpp
+++ compitini/template/main.cpp
@@ -1,6 +1,7 @@
-//15/01.24 by Matteo Fava
-//Esame ##
+//23/01.24 by Matteo Fava
 
+#include "lib/functions.h"
+#include "lib/gplot++.h"
 #include <iostream>
 #include <format>
 #include <cmath>
