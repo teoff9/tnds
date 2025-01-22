861561c825c62bca62fd19b1739262baa83eb65a
Matteo Fava <matteo.luca.fava@gmail.com>
2025-01-22 17:21:02 +0100

    esame e tolab.txt

--- compitini/esame/main.cpp
+++ compitini/esame/main.cpp
@@ -1,10 +1,12 @@
 //23/01.24 by Matteo Fava
+// Esame
 
 #include "lib/functions.h"
 #include "lib/gplot++.h"
 #include <iostream>
 #include <format>
 #include <cmath>
+#include <cassert>
 using namespace std;
 
 int main() {
